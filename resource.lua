536adef911720f6f8c85e6b1e3a6d64b7a258e76
Mythic <61439974+MythicGaming5900@users.noreply.github.com>
2020-08-02 17:54:36 -0600

    Update resource.lua

--- resource.lua
+++ resource.lua
@@ -3,7 +3,5 @@
 --Discord: https://discord.gg/bhwyhYk        |
 ----------------------------------------------
 
-
-resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
-
 client_script "client.lua"
+
